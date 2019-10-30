801882a25abbb003556c97dd40cba5e04dac458f
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2019-10-31 01:47:50 +0200

    Reporting On which days did more than 1% of requests lead to errors?

--- log-analysis.py
+++ log-analysis.py
@@ -1,9 +1,11 @@
+import datetime
 import psycopg2
 
 conn = psycopg2.connect(host='localhost', database='news',
                         user='Mahmoud', password='192168-Water')
 
 cur = conn.cursor()
+
 
 # What are the most popular three articles of all time?
 cur.execute("""select lead, count(*) as times
@@ -35,5 +37,26 @@ print('Who are the most popular article authors of all time?')
 for row in result:
     print('* ' + row[0] + ' - ' + str(row[1]) + ' views')
 print('\n')
+
+
+# On which days did more than 1% of requests lead to errors?
+cur.execute("""create view log_all as
+               select date(time) as day, count(*) as times from log
+	           group by date(time);""")
+cur.execute("""create view log_error as
+               select date(time) as day, count(*) as times from log
+               where status like '404%'
+	           group by date(time);""")
+cur.execute("""select log_all.day, log_all.times, log_error.times
+	           from log_all join log_error
+	           on log_all.day = log_error.day
+	           where (log_error.times::float / log_all.times)*100 > 1;""")
+result = cur.fetchall()
+
+print("On which days did more than 1% of requests lead to errors?")
+for row in result:
+    print('* ' + row[0].strftime("%B %d, %Y") + ' - ' + str((float(row[2])/row[1])*100) + "% errors")
+print('\n')
+
 
 conn.close()
